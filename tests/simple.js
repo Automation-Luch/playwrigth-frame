37c7e56f8216ba9ab2aa3a764d5f00a23dd9147e
AlexeyLuch <pochtamoyapochta123@gmail.com>
2020-11-26 10:00:44 +0200

    firs successe test

--- tests/simple.js
+++ tests/simple.js
@@ -1,26 +1,17 @@
-const playwright = require("playwright");
+const { chromium } = require("playwright");
+const expect = require("chai").expect;
 
 const config = require("../lib/config");
-const click = require("../lib/helpers").click;
-const typeText = require("../lib/helpers").typeText;
 const loadUrl = require("../lib/helpers").loadUrl;
-const waitForText = require("../lib/helpers").waitForText;
-const pressKey = require("../lib/helpers").pressKey;
 const shouldExist = require("../lib/helpers").shouldExist;
-const shouldNotExist = require("../lib/helpers").shouldNotExist;
-
-const generateID = require("../lib/utils").generateID;
-const generateEmail = require("../lib/utils").generateEmail;
-const generateNumbers = require("../lib/utils").generateNumbers;
-
-const utils = require("../lib/utils");
 
 describe("First test", () => {
   let browser;
   let page;
 
   before(async function () {
-    browser = await playwright.launch({
+    browser = await chromium.launch();
+    ({
       headless: config.isHeadless,
       slowMo: config.slowMo,
       devtools: config.devTools,
@@ -28,7 +19,7 @@ describe("First test", () => {
     });
     page = await browser.newPage();
     await page.setDefaultTimeout(config.waitingTimeout);
-    await page.setViewport({
+    await page.setViewportSize({
       width: config.viewportWidth,
       height: config.viewportHeight,
     });
@@ -39,43 +30,14 @@ describe("First test", () => {
   });
 
   it("My first step", async () => {
-    await loadUrl(page, config.baseUrl);
-    await shouldExist(page, "#nav-search");
+    await page.goto("https://www.google.com/", { waitUntil: "networkidle0" });
+
+    await shouldExist(page, '[name="q"]');
 
     const url = await page.url();
     const title = await page.title();
 
-    expect(url).to.contain("dev");
-    expect(title).to.contains("Community");
-  });
-
-  it("browser reload", async () => {
-    await page.reload();
-    await shouldExist(page, "#page-content");
-
-    await waitForText(page, "body", "WRITE A POST");
-
-    const url = await page.url();
-    const title = await page.title();
-
-    //await page.waitFor(1000)
-
-    expect(url).to.contain("dev");
-    expect(title).to.contains("Community");
-  });
-
-  it("click method", async () => {
-    await page.goto("https://dev.to/");
-    await click(page, "#write-link");
-    await shouldExist(page, ".registration-rainbow");
-  });
-
-  it("submit searchbox", async () => {
-    await page.goto(config.baseUrl);
-    // await typeText(page, 'JavaScript', '#nav-search')
-    await typeText(page, utils.generateNumbers(), "#nav-search");
-    await page.waitFor(3000);
-    await pressKey(page, "Enter");
-    await shouldExist(page, "#articles-list");
+    expect(url).to.contain("google");
+    expect(title).to.contains("Google");
   });
 });
